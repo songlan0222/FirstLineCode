e939c7718304ae29aecb11fffd98362d7d629625
songlan0222 <651749248@qq.com>
2020-07-25 19:08:48 +0800

    use ProgressBar with button to increase the num of progressBar, then use AlertDialog but it's hazy about the setPositiveButton and setNegativeButton in AlertDialog.

--- UIWidgetTest/app/src/main/java/com/songlan/uiwidgettest/MainActivity.kt
+++ UIWidgetTest/app/src/main/java/com/songlan/uiwidgettest/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.songlan.uiwidgettest
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -11,6 +13,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener(this)
+        changeImgBtn.setOnClickListener (this)
+        progressBtn.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -19,6 +23,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val inputText = editText.text.toString()
                 val msg = if(inputText.isEmpty())"点锤子你点" else inputText
                 Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+            }
+            R.id.changeImgBtn -> {
+                imageView.setImageResource(R.drawable.image_desktop)
+            }
+            R.id.progressBtn -> {
+                if (horizontalProgressBar.progress < 100){
+                    horizontalProgressBar.progress = horizontalProgressBar.progress+10
+                    if(progressBar.visibility == View.VISIBLE) progressBar.visibility = View.GONE
+                    else progressBar.visibility = View.VISIBLE
+                } else{
+                    AlertDialog.Builder(this).apply {
+                        setTitle("重要提示")
+                        setMessage("进度条已满，继续点击JJ将会缩短")
+                        setCancelable(false)
+                        setPositiveButton("确认继续点击"){dialog, which->
+                            {
+                                Toast.makeText(this@MainActivity, "jj真的短了10cm吧？", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        setNegativeButton("算了算了"){dialog, which->
+                            {
+                                Toast.makeText(this@MainActivity, "幸好怂了，不然真的会短！", Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+                        show()
+                    }
+                    progressBar.visibility = View.GONE
+                }
+
+
+
             }
         }
     }
