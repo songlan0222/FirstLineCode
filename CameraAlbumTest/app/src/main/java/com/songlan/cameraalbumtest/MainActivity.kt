5a16d697d056776b04a910be83c8a7708168d25e
songlan0222 <651749248@qq.com>
2020-09-01 22:41:19 +0800

    创建播放多媒体文件的项目

--- CameraAlbumTest/app/src/main/java/com/songlan/cameraalbumtest/MainActivity.kt
+++ CameraAlbumTest/app/src/main/java/com/songlan/cameraalbumtest/MainActivity.kt
@@ -10,30 +10,34 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
 import androidx.core.content.FileProvider
+import androidx.core.net.toFile
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val takePhoto = 1
-    private lateinit var outputImage: File
+    private val takePhotograph = 2
     private lateinit var imgUri: Uri
+    private lateinit var outputImage: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         photoBtn.setOnClickListener(this)
+        photographBtn.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
         when (p0?.id) {
             R.id.photoBtn -> {
-                outputImage = File(externalCacheDir, "output_img.jpg")
+                outputImage = File(externalCacheDir, "output_image.jpg")
                 if (outputImage.exists()) {
                     outputImage.delete()
                 }
@@ -52,6 +56,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val intent = Intent("android.media.action.IMAGE_CAPTURE")
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri)
                 startActivityForResult(intent, takePhoto)
+
+            }
+            R.id.photographBtn -> {
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.type = "image/*"
+                startActivityForResult(intent, takePhotograph)
             }
         }
     }
@@ -63,26 +74,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 if (resultCode == Activity.RESULT_OK) {
                     val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imgUri))
                     imageView.setImageBitmap(rotateIfRequired(bitmap))
-                } else {
-                    Toast.makeText(this, "你放弃了拍摄照片", Toast.LENGTH_SHORT).show()
                 }
+            }
+            takePhotograph -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    data.data?.let {uri ->
+                        val bitmap = getBitmapFromUri(uri)
+                        imageView.setImageBitmap(bitmap)
+                    }
+                }
+
             }
         }
     }
+
+    private fun getBitmapFromUri(uri: Uri) =
+        contentResolver.openFileDescriptor(uri, "r").use {
+            BitmapFactory.decodeFileDescriptor(it?.fileDescriptor)
+        }
 
     private fun rotateIfRequired(bitmap: Bitmap): Bitmap {
         val exif = ExifInterface(outputImage.path)
         val orientation =
             exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
         return when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90)
-            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180)
-            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270)
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateBimap(bitmap, 90)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBimap(bitmap, 180)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBimap(bitmap, 270)
             else -> bitmap
         }
     }
 
-    private fun rotateBitmap(bitmap: Bitmap, degree: Int): Bitmap {
+    private fun rotateBimap(bitmap: Bitmap, degree: Int): Bitmap {
         val matrix = Matrix()
         matrix.postRotate(degree.toFloat())
         val rotatedBitmap =
@@ -90,6 +113,4 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         bitmap.recycle()
         return rotatedBitmap
     }
-
-
 }
--- NoticeApplication/app/src/main/java/com/songlan/noticeapplication/MainActivity.kt
+++ NoticeApplication/app/src/main/java/com/songlan/noticeapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.songlan.noticeapplication
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -23,36 +24,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         createNoticeChannel()
-
         noticeBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.noticeBtn -> {
+                val intent = Intent(this, NotificationActivity::class.java)
+                intent.putExtra("text", "在新Activity中响应的内容")
+                val pi = PendingIntent.getActivity(this, 0, intent, 0)
+                val notification = NotificationCompat.Builder(this, "normal").apply {
+                    setContentTitle("通知的标题")
+                    setStyle(
+                        NotificationCompat.BigPictureStyle().bigPicture(
+                            BitmapFactory.decodeResource(
+                                resources,
+                                R.drawable.big_image
+                            )
+                        )
+                    )
+                    setSmallIcon(R.drawable.small_icon)
+                    setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
+                    setAutoCancel(true)
+                    setContentIntent(pi)
+                }.build()
+                manager.notify(1, notification)
+            }
+        }
     }
 
     private fun createNoticeChannel(){
         manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             val normalChannel = NotificationChannel("normal", "普通通知", NotificationManager.IMPORTANCE_DEFAULT)
-            val importantChannel = NotificationChannel("important", "重要通知", NotificationManager.IMPORTANCE_HIGH)
             manager.createNotificationChannel(normalChannel)
-            manager.createNotificationChannel(importantChannel)
-        }
-    }
-
-    override fun onClick(view: View?) {
-        when(view?.id){
-            R.id.noticeBtn -> {
-                val intent = Intent(this, NotificationActivity::class.java)
-                intent.putExtra("text", "随便发送点内容，象征性的展示一下")
-                val pi = PendingIntent.getActivity(this, 0, intent, 0)
-                val notification = NotificationCompat.Builder(this, "normal").apply {
-                    setContentTitle("标准通知")
-                    setStyle(NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(resources, R.drawable.big_image)))
-                    setAutoCancel(true)
-                    setContentIntent(pi)
-                    setSmallIcon(R.drawable.small_icon)
-                    setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
-                }.build()
-                manager.notify(1, notification)
-            }
         }
     }
 }
