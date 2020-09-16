6d5eeb24e261c7e77aadfdcd6c9cb923a91f4101
songlan0222 <651749248@qq.com>
2020-09-16 14:43:13 +0800

    初始化MDTest项目

---
+++ MDTest/app/src/main/java/com/songlan/mdtest/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.songlan.mdtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
