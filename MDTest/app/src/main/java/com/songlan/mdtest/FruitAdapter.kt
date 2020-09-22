99af19446bf3e1ef5c6782db85a4f3c740afb020
songlan0222 <651749248@qq.com>
2020-09-22 16:21:49 +0800

    基本掌握以下组件的基本使用 - Toolbar，工具栏 - DrawerLayout, 抽屉布局 - NavigationLayout, 抽屉菜单 - FloatingActionButton，浮动按钮 - Snackbar，功能对话框 - CoordinatorLayout， 以上两个组件的交互 - RecyclerView，循环展示组件 - SwipeRefreshLayout，下拉刷新

---
+++ MDTest/app/src/main/java/com/songlan/mdtest/FruitAdapter.kt
@@ -0,0 +1,36 @@
+package com.songlan.mdtest
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class FruitAdapter(val context: Context, val data: List<Fruit>): RecyclerView.Adapter<FruitAdapter.ViewHolder>() {
+    inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val fruitName: TextView = view.findViewById(R.id.fruitName)
+        val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item, parent, false)
+        val viewHolder = ViewHolder(view)
+        return viewHolder
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val fruit = data[position]
+        holder.fruitName.text = fruit.name
+        Glide.with(context).load(fruit.imageId).into(holder.fruitImage)
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
